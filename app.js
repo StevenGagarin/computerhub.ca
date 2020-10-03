0b0dbca36f6dfdeee57851f7b074a84958eb1240
stevekiller176 <38894178+stevekiller176@users.noreply.github.com>
2020-10-02 22:21:28 -0500

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 100,
+        "value": 50,
         "density": {
           "enable": false,
           "value_area": 90
